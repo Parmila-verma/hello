615962f9b620131ef0e977105a448e03c437962e
unknown <you@example.com>
2025-05-09 10:52:54 +0530

    accordians

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-    <script defer src="https://cdn.jsdelivr.net/npm/@finsweet/attributes-copyclip@1/copyclip.js"></script>
+
+<script>
+<script defer src="https://cdn.jsdelivr.net/npm/@finsweet/attributes-copyclip@1/copyclip.js"></script>
+
+<script defer src="https://cdn.jsdelivr.net/npm/@finsweet/attributes-accordion@1/accordion.js"></script>
+</script>
