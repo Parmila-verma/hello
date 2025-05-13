10f6e0254674c5b2a45ebc39d8d9d9cf45aae4d5
unknown <you@example.com>
2025-05-13 16:05:17 +0530

    first

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-
+  
 <script>
-<script defer src="https://cdn.jsdelivr.net/npm/@finsweet/attributes-copyclip@1/copyclip.js"></script>
+    const container = document.getElementById('hoverImage');
+    const image = container.querySelector('img');
 
-<script defer src="https://cdn.jsdelivr.net/npm/@finsweet/attributes-accordion@1/accordion.js"></script>
+      container.addEventListener('mousemove', (e) => {
+          const rect = container.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const moveX = (x - rect.width / 2) / 20;
+    const moveY = (y - rect.height / 2) / 20;
+
+    image.style.transform = `scale(1.05) translate(${moveX}px, ${moveY}px)`;
+      });
+
+      container.addEventListener('mouseleave', () => {
+        image.style.transform = 'scale(1) translate(0, 0)';
+      });
 </script>
